4425bf888e7e17d1cbe998d612c3c4ffc3552a7b
minghong.kang <minghong.kang@iluvatar.com>
2025-07-03 13:01:51 +0800

    fx

--- ci/debug_pipeline_kenny.groovy
+++ ci/debug_pipeline_kenny.groovy
@@ -1,49 +1,37 @@
-pipeline {
-    agent none
-    stages {
-        stage('Start vLLM Servers') {
-            parallel {
-                stage('host1') {
-                    agent { label 'BI150-X86-U-1.45' }
-                    options {
-                        ansiColor('xterm')
-                    }
-                    stages {
-                        stage('Test1') {
-                            steps {
-                                sh """
-                                    sleep 12000
-                                """
-                            }
-                        }
-                    }
-                    post {
-                        always {
-                            echo 'host1 post'
-                        }
-                    }
-                }
+def NODE_LABEL_MASTER = "BI150-X86-U-1.45"
+def NODE_LABEL_SLAVE = "BI150-X86-U-1.46"
+def DOCKER_IMAGE = "10.150.9.98:80/devops_tools/corex-testagent-ubuntu-x86_64:master"
 
-                stage('host2') {
-                    agent { label 'BI150-X86-U-1.46' }
-                    options {
-                        ansiColor('xterm')
-                    }
-                    stages {
-                        stage('Test2') {
-                            steps {
+pipeline {
+    agent NODE_LABEL_SLAVE
+    stages {
+        stage("vllm_test") {
+            steps {
+                script {
+
+                    sh """
+                        docker run -itd -v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P --name corex-testagent 10.150.9.98:80/devops_tools/corex-testagent-ubuntu-x86_64:master
+                    """
+
+                    node(NODE_LABEL_MASTER) {
+                        script{
+                            def dockerImage = docker.image(DOCKER_IMAGE)
+                            dockerImage.pull()
+                            dockerImage.inside("-v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P") {
                                 sh """
                                     sleep 12000
                                 """
                             }
                         }
                     }
-                    post {
-                        always {
-                            echo 'host2 post'
-                        }
-                    }
                 }
+            }
+        }
+    }
+    post{
+        always{
+            script{
+                cleanWs()
             }
         }
     }
