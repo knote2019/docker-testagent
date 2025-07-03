3cbe91126cf7818eb8f7c3c6b4cf8bf972c4a9db
minghong.kang <minghong.kang@iluvatar.com>
2025-07-03 13:08:56 +0800

    fx

--- ci/debug_pipeline_kenny.groovy
+++ ci/debug_pipeline_kenny.groovy
@@ -1,6 +1,7 @@
 def NODE_LABEL_MASTER = "BI150-X86-U-1.45"
 def NODE_LABEL_SLAVE = "BI150-X86-U-1.46"
 def DOCKER_IMAGE = "10.150.9.98:80/devops_tools/corex-testagent-ubuntu-x86_64:master"
+def dockerImage = docker.image(DOCKER_IMAGE)
 
 pipeline {
     agent { node { label NODE_LABEL_SLAVE } }
@@ -8,7 +9,6 @@ pipeline {
         stage('run_test') {
             steps {
                 script{
-                    def dockerImage = docker.image(DOCKER_IMAGE)
                     dockerImage.pull()
                     dockerImage.inside("-v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P") {
                         sh """
@@ -16,7 +16,6 @@ pipeline {
                         """
                         node(NODE_LABEL_MASTER) {
                             script{
-                                def dockerImage = docker.image(DOCKER_IMAGE)
                                 dockerImage.pull()
                                 dockerImage.inside("-v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P") {
                                     sh """
