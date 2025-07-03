db520057fc96cf1392fb515f4f03833c905a65e3
minghong.kang <minghong.kang@iluvatar.com>
2025-07-03 12:05:32 +0800

    fx

---
+++ ci/debug_pipeline_kenny.groovy
@@ -0,0 +1,60 @@
+def CI_NODE_AMD64_LABEL = params.CI_NODE_AMD64_LABEL.trim()
+
+/*****************************************************************
+ * common.
+ * **************************************************************/
+def build_torch_testagent = params.build_torch_testagent
+def build_vllm_testagent = params.build_vllm_testagent
+
+pipeline {
+    agent none
+    stages {
+        stage('x86_64'){
+            agent { node { label CI_NODE_AMD64_LABEL } }
+            options {
+                timestamps ()
+                ansiColor('xterm')
+            }
+            stages {
+                /*****************************************************************
+                 * torch.
+                 * **************************************************************/
+                stage('torch') {
+                    when { expression { params.build_torch_testagent == true } }
+                    steps {
+                        script{
+                            sh """
+                                cd ${env.WORKSPACE}/framework
+                                docker build --build-arg FORCE_BUILD=\$(date +%s) --force-rm --tag 10.150.9.98:80/devops_tools/torch-v2.4.1:kenny --file torch-v2.4.1.dockerfile .
+                                docker push 10.150.9.98:80/devops_tools/torch-v2.4.1:kenny
+                                docker images | grep devops_tools
+                            """
+                        }
+                    }
+                }
+
+                /*****************************************************************
+                 * vllm.
+                 * **************************************************************/
+                stage('vllm') {
+                    when { expression { params.build_vllm_testagent == true } }
+                    steps {
+                        script{
+                            sh """
+                                cd ${env.WORKSPACE}/framework
+                                docker build --build-arg FORCE_BUILD=\$(date +%s) --force-rm --tag 10.150.9.98:80/devops_tools/vllm-v0.6.3:kenny --file vllm-v0.6.3.dockerfile .
+                                docker push 10.150.9.98:80/devops_tools/vllm-v0.6.3:kenny
+                                docker images | grep devops_tools
+                            """
+                        }
+                    }
+                }
+            }
+            post{
+                always{
+                    cleanWs()
+                }
+            }
+        }
+    }
+}
