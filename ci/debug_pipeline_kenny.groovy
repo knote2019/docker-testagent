ee4e6a40a0433c7c3be53cc08bfaf732d433b945
minghong.kang <minghong.kang@iluvatar.com>
2025-07-03 13:07:48 +0800

    fx

--- ci/debug_pipeline_kenny.groovy
+++ ci/debug_pipeline_kenny.groovy
@@ -5,24 +5,29 @@ def DOCKER_IMAGE = "10.150.9.98:80/devops_tools/corex-testagent-ubuntu-x86_64:ma
 pipeline {
     agent { node { label NODE_LABEL_SLAVE } }
     stages {
-        stage("vllm_test") {
+        stage('run_test') {
             steps {
-                script {
-
-                    sh """
-                        docker run -itd -v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P --name corex-testagent 10.150.9.98:80/devops_tools/corex-testagent-ubuntu-x86_64:master
-                    """
-
-                    node(NODE_LABEL_MASTER) {
-                        script{
-                            def dockerImage = docker.image(DOCKER_IMAGE)
-                            dockerImage.pull()
-                            dockerImage.inside("-v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P") {
-                                sh """
-                                    sleep 12000
-                                """
+                script{
+                    def dockerImage = docker.image(DOCKER_IMAGE)
+                    dockerImage.pull()
+                    dockerImage.inside("-v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P") {
+                        sh """
+                            echo "kkk"
+                        """
+                        node(NODE_LABEL_MASTER) {
+                            script{
+                                def dockerImage = docker.image(DOCKER_IMAGE)
+                                dockerImage.pull()
+                                dockerImage.inside("-v /dev:/dev -v /lib/modules:/lib/modules --privileged --shm-size 64g -v /stores:/stores -P") {
+                                    sh """
+                                        sleep 12000
+                                    """
+                                }
                             }
                         }
+                        sh """
+                            echo "mmm"
+                        """
                     }
                 }
             }
