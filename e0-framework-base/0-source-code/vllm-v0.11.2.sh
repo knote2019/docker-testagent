5c63568a94609b8594fa5b8ccfc31bb695ef9a19
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-11-26 18:25:29 +0800

    fix

--- e0-framework-base/0-source-code/vllm-v0.11.2.sh
+++ e0-framework-base/0-source-code/vllm-v0.11.2.sh
@@ -7,40 +7,10 @@ git checkout v0.11.2 -b master
 git remote set-url origin git@github.com:knote2019/vllm-v0.11.2.git
 git push origin master
 
-rm -rf benchmarks/
-rm -rf .buildkite/
-rm -rf .clang-format
-rm -rf CODE_OF_CONDUCT.md
-rm -rf collect_env.py
-rm -rf CONTRIBUTING.md
-rm -rf Dockerfile*
-rm -rf .dockerignore
-rm -rf docs/
-rm -rf examples/
-rm -rf find_cuda_init.py
-rm -rf format.sh
-rm -rf .github/
-rm -rf LICENSE
-rm -rf python_only_dev.py
-rm -rf README.md
-rm -rf .readthedocs.yaml
-rm -rf requirements-cpu.txt
-rm -rf requirements-dev.txt
-rm -rf requirements-hpu.txt
-rm -rf requirements-lint.txt
-rm -rf requirements-neuron.txt
-rm -rf requirements-openvino.txt
-rm -rf requirements-rocm.txt
-rm -rf requirements-test.in
-rm -rf requirements-test.txt
-rm -rf requirements-tpu.txt
-rm -rf requirements-xpu.txt
-rm -rf SECURITY.md
-rm -rf tests/
-rm -rf tools/
+TODO: clean
+
 python use_existing_torch.py
 rm -rf use_existing_torch.py
-rm -rf .yapfignore
 
 git add .
 git commit -m "clean"
